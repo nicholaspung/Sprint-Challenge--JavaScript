5fe1d887361992ba7ffaf834a1ed9c3b17415bd0
Nicholas Pung <nicholaspung>
2019-05-10 09:28:39 -0700

    WIP:copy pasted prototypes for refactoring in classes

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,25 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+function CuboidMaker(attributes) {
+    this.lengthh = attributes.lengthh;
+    this.width = attributes.width;
+    this.height = attributes.height;
+}
+
+CuboidMaker.prototype.volume = function() {
+return this.lengthh * this.width * this.height;
+}
+
+CuboidMaker.prototype.surfaceArea = function() {
+    return 2 * (this.lengthh * this.width + this.lengthh * this.height + this.width * this.height);
+}
+
+const cuboid = new CuboidMaker({
+    lengthh: 4,
+    width: 5,
+    height: 5
+})
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
